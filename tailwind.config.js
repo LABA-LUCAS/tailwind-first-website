5b8d95f259928884d00a9ebafd439d0985372303
Lucas Wienen <zwartekikker2001@gmail.com>
2024-11-13 15:28:27 +0100

    Niet tevrede

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
